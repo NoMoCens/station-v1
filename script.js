8b69530cd193f8cbdcdc5e8834aa01d962f9289d
Newfie <157941859+Newfies@users.noreply.github.com>
2025-04-10 22:43:41 -0500

    sd

--- script.js
+++ script.js
@@ -92,6 +92,10 @@ document.addEventListener("DOMContentLoaded", (event) => {
                 if (loc === "I2xvYWQ2NA=="){
                     document.body.innerHTML = '<iframe src="https://nomocens.github.io/station-selection-1/sm64/index.html" style="position:fixed; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;"></iframe>'
                 }
+
+                if (loc === "I0ZJWA=="){
+                    alert('Right click to open this in a fixed version that seems to fix setting and other activities.');
+                }
                 
                 else {
                     // Decode loc and navigate to the decoded URL
